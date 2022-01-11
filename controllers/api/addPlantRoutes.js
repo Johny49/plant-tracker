fca7d921b91a8ac5ddbdeaea413ae386fb0f589b
jacob taylor <jacobtaylorclt@gmail.com>
2022-01-11 14:50:57 -0500

    API axios stuff

--- controllers/api/addPlantRoutes.js
+++ controllers/api/addPlantRoutes.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const {Plant} = require('../../models');
+const axios = require('axios'); 
 
-router.post('/', async (req, res) => {
-    try {
+router.post('/:name', async (req, res) => {
+    try { 
+        console.log("HERE")
+        const result = await axios.get(`https://api.inaturalist.org/v1/taxa?q=${req.params.name}`);
+        // console.log(result.data.result.results);
+        return res.json(result.data.results[0]);
+        
         const plant = await Plant.create(req.body); //NOT SURE YET OF WHAT GOING TO BE ADDED
         return res.json(plant)
     } catch (err) {
--- models/plant.js
+++ models/plant.js
@@ -1,6 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-const request = require('request');
 
 class Plant extends Model {}
 
@@ -12,6 +11,7 @@ Plant.init(
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      primaryKey: true 
     },
     name: {
       type: DataTypes.STRING,
