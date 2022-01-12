d5d21b3f95258a9a957957da12121fbd88f7bc8f
jacob taylor <jacobtaylorclt@gmail.com>
2022-01-12 14:24:10 -0500

    fake seed data

--- models/notes.js
+++ models/notes.js
@@ -10,7 +10,7 @@ Notes.init(
             allowNull:false,
             primaryKey: true
         },
-        name: {
+        nickname: {
             type: DataTypes.INTEGER,
             allowNull:false,
             primaryKey: true
