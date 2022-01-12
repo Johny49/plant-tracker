8ea182325931676e6e254033a9e15f2edeb48268
jacob taylor <jacobtaylorclt@gmail.com>
2022-01-12 13:23:29 -0500

    adding 1to1 and 1tomany

--- models/index.js
+++ models/index.js
@@ -1,7 +1,28 @@
 const User = require('./User');
 const Plant = require('./Plant');
+const Notes = require('./Notes')
 
 
+User.hasOne(Notes, {
+  foreignKey: 'User_id',
+
+  onDelete: 'CASCADE',
+});
 
 
-module.exports = { User, Plant };
+Notes.belongsTo(User, {
+  foreignKey: 'User_id',
+});
+
+User.hasMany(Plant, {
+    foreignKey: 'User_id',
+    onDelete: 'CASCADE',
+  });
+  
+
+  Plant.belongsTo(User, {
+    foreignKey: 'User_id',
+  });
+
+
+module.exports = { User, Plant, Notes };
