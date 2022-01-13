b28f347b303face43d0ff17004a48008cd3acb1d
jacob taylor <jacobtaylorclt@gmail.com>
2022-01-13 10:25:12 -0500

    update fake seed data

--- seeds/plantSeedData.js
+++ seeds/plantSeedData.js
@@ -3,25 +3,16 @@ const {Plant} = require('../models');
 const plantData =[
     {
         "id":"1434234234",
-        "nickname":"jigglypoof",
-        "note":"caught this bad boy in mt moon",
-        "user_id":"2345234",
-        "plant_id":"235235"
+        "plant":"Fern"
     },
     {
         "id":"5678678",
-        "nickname":"Boris",
-        "note":"favorite pink plant",
-        "user_id":"273284",
-        "plant_id":"098477"
+        "plant":"Hibicus"
 
     },
     {
         "id":"14340982",
-        "nickname":"Michael",
-        "note":"named after my ex",
-        "user_id":"2095252",
-        "plant_id":"8907552"
+        "plant":"Lion's Mane"
 
     }
 ]
