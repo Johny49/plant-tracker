c8772d542b36adca8ca12c83ccbe51576ba8c71c
Wesley Rogers <wprogers86@gmail.com>
2022-01-13 13:24:12 -0500

    updated plant handlebar

--- controllers/plantsRoutes.js
+++ controllers/plantsRoutes.js
@@ -1,14 +1,21 @@
 const router = require('express').Router();
 const {Plant} = require('../models');
+const axios = require('axios'); 
 
 
 router.get('/', async (req, res) => {
     try {
+        const plantApi = async (name) => {
+            const result = await axios.get(`https://api.inaturalist.org/v1/taxa?q=${name}`);
+            return result;
+        }
         
+        // console.log(result.data.result.results);
+        // return res.json(result.data.results[0]);
         const plantsData = await Plant.findAll({});
         const plants = plantsData.map((plant) => plant.get({plain: true}))
-        console.log("b")
-        res.render('plant', {plants})
+        
+        res.render('plant', {plants, plantApi})
     } catch (err){
         res.status(500).json(err);
     }
