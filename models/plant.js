f830257f9c422f796d25943aa07d4be1d9825606
Wesley Rogers <wprogers86@gmail.com>
2022-01-13 23:09:18 -0500

    added add.js, updated seeds data, updated plant route

--- models/plant.js
+++ models/plant.js
@@ -3,20 +3,35 @@ const sequelize = require('../config/connection');
 
 class Plant extends Model {}
 
-
-
-
 Plant.init(
   {
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      primaryKey: true 
+      primaryKey: true,
+      autoIncrement: true, 
     },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    nickname: {
+      type: DataTypes.STRING,
+      allowNull:false
+  },
+  note: {
+      type: DataTypes.STRING,
+      allowNull:false
+  },
+  user_id: {
+      type: DataTypes.INTEGER,
+      allowNull:false,
+      unique: true,
+      references: {
+          model: 'user',
+          key: 'id',
+      }
+  },
   },
   {
     sequelize,
--- public/js/login.js
+++ public/js/login.js
@@ -25,9 +25,7 @@ const signupFormHandler = async (event) => {
   const username = document.querySelector('#username-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
-console.log(username);
-console.log(email);
-console.log(password);
+
   if (username && email && password) {
     const response = await fetch('/api/user', {
       method: 'POST',
